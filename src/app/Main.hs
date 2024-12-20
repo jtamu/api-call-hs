a7cc8ce190c3c3afbef8bb3bea580009cbd1692b
jtamu <gtamu.dip@gmail.com>
2024-12-21 00:08:56 +0900

    feat: haskell api call

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Main where
 
+import Data.Aeson (FromJSON, Value (Object), decode, parseJSON, (.:))
+import qualified Data.Aeson.Key as Key
+import Data.Aeson.Types (Parser)
+import Network.HTTP.Simple (getResponseBody, httpLbs, parseRequest_)
+
+-- import qualified Data.ByteString.Lazy as LBS
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  -- maybeValue <- decode <$> LBS.readFile "test.json"
+  -- case (maybeValue :: Maybe IPRes) of
+  --   Nothing -> putStrLn "parsing failed"
+  --   Just value -> print value
+
+  res <- httpLbs $ parseRequest_ "https://httpbin.org/ip"
+  let ipMaybe = decode (getResponseBody res)
+  case (ipMaybe :: Maybe IPRes) of
+    Nothing -> putStrLn "parsing failed"
+    Just ip -> putStrLn $ "your ip:" ++ origin ip
+
+newtype IPRes = IPRes {origin :: String} deriving (Show)
+
+instance FromJSON IPRes where
+  parseJSON :: Value -> Parser IPRes
+  parseJSON (Object v) = IPRes <$> v .: Key.fromString "origin"
+  parseJSON _ = mempty
