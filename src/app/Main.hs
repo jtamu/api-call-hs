24b8c0b860dd9fca79c5b78855af7f30fac5d7bd
jtamu <gtamu.dip@gmail.com>
2024-12-21 03:24:52 +0900

    refactor: httpJSONEitherで安全にデコードまでやる

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,9 +3,9 @@
 
 module Main where
 
-import Data.Aeson (FromJSON, decode)
+import Data.Aeson (FromJSON)
 import GHC.Generics (Generic)
-import Network.HTTP.Simple (getResponseBody, httpLBS, httpLbs, parseRequest_, setRequestHeader)
+import Network.HTTP.Simple (getResponseBody, httpJSONEither, parseRequest_, setRequestHeader)
 
 -- import qualified Data.ByteString.Lazy as LBS
 
@@ -16,23 +16,23 @@ main = do
   --   Nothing -> putStrLn "parsing failed"
   --   Just value -> print value
 
-  res <- httpLbs $ parseRequest_ "https://httpbin.org/ip"
-  let ipMaybe = decode (getResponseBody res)
-  case (ipMaybe :: Maybe IPRes) of
-    Nothing -> putStrLn "parsing failed"
-    Just ip_a -> do
+  res <- httpJSONEither "https://httpbin.org/ip"
+  let ipEither = getResponseBody res
+  case ipEither of
+    Left msg -> print msg
+    Right ip_a -> do
       putStrLn $ "your ip:" ++ origin ip_a
 
       let infoReq =
             parseRequest_ ("https://ipapi.co/" ++ origin ip_a ++ "/json/")
               -: setRequestHeader "User-Agent" ["My Haskell App/1.0"]
 
-      infoRes <- httpLBS infoReq
+      infoRes <- httpJSONEither infoReq
 
-      let infoMaybe = decode (getResponseBody infoRes)
-      case (infoMaybe :: Maybe IPInfo) of
-        Nothing -> putStrLn "parsing failed"
-        Just info -> print info
+      let infoEither = getResponseBody infoRes
+      case infoEither of
+        Left msg -> print msg
+        Right info -> print (info :: IPInfo)
 
 (-:) :: t1 -> (t1 -> t2) -> t2
 x -: f = f x
